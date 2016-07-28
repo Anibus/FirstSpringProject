175b5acb0c6b8eef6524b4c140d50bc8b354345e
Anibus <anibus100@gmail.com>
2016-07-28 10:06:42 -0700

    Add files via upload

---
+++ IhearyahMain/src/main/java/com/ihearyah/IhearyahMainApplication.java
@@ -0,0 +1,12 @@
+package com.ihearyah;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IhearyahMainApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IhearyahMainApplication.class, args);
+	}
+}
